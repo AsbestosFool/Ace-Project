b311714a6428556b4c38a6468df03cba99ce37f5
Mike Alamil <asbestosfool@gmail.com>
2025-05-08 13:19:30 -0500

    added switch case

--- Chapter 3/sandbox.js
+++ Chapter 3/sandbox.js
@@ -69,3 +69,21 @@ for(let i = 0; i < scores.length; i++){
         break;
     }
 }
+
+const grade = 'D';
+switch(grade){
+    case 'A':
+        console.log('you got an A');
+        break;
+    case 'B':
+        console.log('you got a B');
+        break;
+    case 'C':
+        console.log('you got a C');
+        break;
+    case 'D':
+        console.log('you got a D');
+        break;
+    default:
+        console.log('not a valid grade');
+}
