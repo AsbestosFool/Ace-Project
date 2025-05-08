4ce48a377a070c5a56cbc92663bb2a33b6fe4f11
Mike Alamil <asbestosfool@gmail.com>
2025-05-08 14:43:04 -0500

    added callbacks and foreach

--- Chapter 4/sandbox.js
+++ Chapter 4/sandbox.js
@@ -29,14 +29,38 @@
 // const area = calcArea(5);
 // console.log(area);
 
-const bill = (products, tax) => {
-    let total = 0;
-    for(let i = 0; i < products.length; i++){
-        total += products[i] + products[i] * tax;
-    }
-    return total;
+// const bill = (products, tax) => {
+//     let total = 0;
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// };
+
+// console.log(bill([10, 15, 30], .2));
+
+// const names = 'shaun';
+
+// const greet = () => 'hello ';
+
+// let resultOne = greet();
+// console.log(resultOne);
+
+// let resultTwo = names.toUpperCase();
+// console.log(resultTwo); 
+
+// const myFunc = (callbackFunc) => {
+//     let value = 50;
+//     callbackFunc(value);
+// };
+// myFunc(val => {
+//     console.log(val);
+// });
+
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+const logPerson = (person, index) => {
+    console.log(`${index} - hello ${person}`);    
 };
 
-
-console.log(bill([10, 15, 30], .2));
-
+people.forEach(logPerson);
