673f7776507a4e65cf8241f46e2af4d38be5f4fc
Mike Alamil <asbestosfool@gmail.com>
2025-05-08 14:57:58 -0500

    added stuff in to ul using js

--- Chapter 4/sandbox.js
+++ Chapter 4/sandbox.js
@@ -57,10 +57,20 @@
 //     console.log(val);
 // });
 
-let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+// const logPerson = (person, index) => {
+//     console.log(`${index} - hello ${person}`);    
+// };
 
-const logPerson = (person, index) => {
-    console.log(`${index} - hello ${person}`);    
-};
+// people.forEach(logPerson);
 
-people.forEach(logPerson);
+const ul = document.querySelector('.people');
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+let html = ``;
+
+ people.forEach(function(person){
+     html += `<li style="color: purple">${person}</li>`;
+ });
+
+ console.log(html);
+ ul.innerHTML = html;
