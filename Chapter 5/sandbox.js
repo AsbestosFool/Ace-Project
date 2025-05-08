a9cfc0f6a221f2ecaf7a085a2c8f275d768b842d
Mike Alamil <asbestosfool@gmail.com>
2025-05-08 15:16:34 -0500

    Set up chapter 5

---
+++ Chapter 5/sandbox.js
@@ -0,0 +1,10 @@
+let user = {
+    name: 'crystal',
+    age: 30,
+    email: 'some@mail.com',
+    location: 'Berlin',
+    blogs:['why mac & cheese rules', '10 things to make with marmite']
+};
+
+console.log(user);
+console.log('hi');
