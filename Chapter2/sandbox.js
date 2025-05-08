c530aa59f0fc12500af1180d455c2f2a4d2df125
Mike Alamil <asbestosfool@gmail.com>
2025-05-08 08:19:31 -0500

    adde variables and used consoles

--- Chapter2/sandbox.js
+++ Chapter2/sandbox.js
@@ -1,1 +1,15 @@
 alert("lel");
+console.log("1");
+console.log("2");
+
+let age = 25;
+let year = 2019
+console.log(age, year);
+age = 30;
+console.log(age);
+
+const point = 100;
+console.log(point);
+
+var score = 75;
+console.log(score);
