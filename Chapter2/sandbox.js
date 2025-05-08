ae41b5ee1ef3da1d70f4ac38d2b49123285abff8
Mike Alamil <asbestosfool@gmail.com>
2025-05-08 08:35:01 -0500

    Added the string tranining for javascript

--- Chapter2/sandbox.js
+++ Chapter2/sandbox.js
@@ -2,6 +2,7 @@ alert("lel");
 console.log("1");
 console.log("2");
 
+// number stuff
 let age = 25;
 let year = 2019
 console.log(age, year);
@@ -13,3 +14,24 @@ console.log(point);
 
 var score = 75;
 console.log(score);
+
+// string stuff
+console.log('hello, world');
+
+let email = "vU9l6@example.com";
+console.log(email);
+
+let firstname ='Brandon';
+let lastname = 'Sanderson';
+let fullname = firstname + ' ' + lastname;
+console.log(fullname);
+
+console.log(fullname[2]);
+console.log(fullname.length);   
+
+console.log(fullname.toUpperCase());
+let result = fullname.toLowerCase();
+console.log(result, fullname);
+
+let index = email.indexOf('@');
+console.log(index);
