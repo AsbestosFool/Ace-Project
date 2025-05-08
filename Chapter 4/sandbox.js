810f47bcd1ed8de474183fb6a86c70554cce4482
Mike Alamil <asbestosfool@gmail.com>
2025-05-08 14:27:14 -0500

    Added arrow functions

--- Chapter 4/sandbox.js
+++ Chapter 4/sandbox.js
@@ -20,10 +20,23 @@
 // speak('shaun');
 // speak('mario', 'morning');
 
-const calcArea = function(radius){
-    return 3.14 * radius ** 2;
+// const calcArea = function(radius){
+//     return 3.14 * radius ** 2;
+// };
+
+// const calcArea = (radius) => 3.14 * radius ** 2;
+
+// const area = calcArea(5);
+// console.log(area);
+
+const bill = (products, tax) => {
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax;
+    }
+    return total;
 };
 
-const area = calcArea(5);
-console.log(area);
+
+console.log(bill([10, 15, 30], .2));
 
