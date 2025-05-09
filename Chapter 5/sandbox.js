58b07403ca5bc55db6b70cda0b3f6b5f2cfcb7e6
Mike Alamil <asbestosfool@gmail.com>
2025-05-09 08:39:20 -0500

    added more to the object literals

--- Chapter 5/sandbox.js
+++ Chapter 5/sandbox.js
@@ -1,18 +1,43 @@
+// const blogs = [
+//     {title: 'why mac & cheese rules', likes: 30},
+//     {title: '10 things to make with marmite', likes: 50}
+// ];
+
+//console.log(blogs);
+
 let user = {
     name: 'crystal',
     age: 30,
     email: 'some@mail.com',
     location: 'Berlin',
-    blogs:['why mac & cheese rules', '10 things to make with marmite']
+    blogs:[
+        {title: 'why mac & cheese rules', likes: 30},
+        {title: '10 things to make with marmite', likes: 50}
+    ],
+    login: function(){
+        console.log('the user logged in');  
+    },   
+    logout: function(){
+        console.log('the user logged out');  
+    },
+    logBlogs: function(){
+        console.log('this user has written the following blogs');
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        })
+    }
 };
 
-console.log(user);
-console.log(user.name);
+user.logBlogs();
+console.log(this);
 
-user.age = 35;
-console.log(user.age);
-console.log(user['email']);
-user['name' ] = 'chun-li';
-console.log(user['name']);
+// console.log(user);
+// console.log(user.name);
 
-console.log(typeof user);
+// user.age = 35;
+// console.log(user.age);
+// console.log(user['email']);
+// user['name' ] = 'chun-li';
+// console.log(user['name']);
+
+// console.log(typeof user);
