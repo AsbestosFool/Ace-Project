839fe6b8e4ee82e614d844f4268ad8af9d7020ac
Mike Alamil <asbestosfool@gmail.com>
2025-05-09 09:45:46 -0500

    added text to html using js

--- Chapter 6/sandbox.js
+++ Chapter 6/sandbox.js
@@ -5,12 +5,31 @@
 // const errors = document.querySelectorAll('error');
 // console.log(errors);
 
-const title = document.getElementById('page-title');
-console.log(title); 
+// const title = document.getElementById('page-title');
+// console.log(title); 
 
-const errors = document.getElementsByClassName('error');
-console.log(errors);
-console.log(errors[0]);
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
 
-const paras = document.getElementsByTagName('p');
-console.log(paras);
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+
+const para = document.querySelector('p');
+// para.innerText = 'ninjas are awesome';
+
+const paras = document.querySelectorAll('p');
+// paras.forEach(para => {
+//     console.log(para.innerText);
+//     para.innerText += ' new text';
+    
+// });
+
+const content = document.querySelector('.content');
+//content.innerHTML += '<h2>hello</h2>';
+
+const people = ['mario', 'luigi', 'yoshi'];
+
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`;
+});
