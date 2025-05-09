935845b60d158ccccb682d4bdd4aa474dc646ade
Mike Alamil <asbestosfool@gmail.com>
2025-05-09 09:30:33 -0500

    added the getElements

--- Chapter 6/sandbox.js
+++ Chapter 6/sandbox.js
@@ -1,6 +1,16 @@
 // const para = document.querySelector('body > h1');
 // console.log(para);
 
-const paras = document.querySelectorAll('p');
-const errors = document.querySelectorAll('error');
+// const paras = document.querySelectorAll('p');
+// const errors = document.querySelectorAll('error');
+// console.log(errors);
+
+const title = document.getElementById('page-title');
+console.log(title); 
+
+const errors = document.getElementsByClassName('error');
 console.log(errors);
+console.log(errors[0]);
+
+const paras = document.getElementsByTagName('p');
+console.log(paras);
