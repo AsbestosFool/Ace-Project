6fe5eda3d26a9e4826f2cf878d52166c6fc35b08
Mike Alamil <asbestosfool@gmail.com>
2025-05-14 13:38:38 -0500

    lifted the state up

--- Next JS Training/react-crash-course/src/components/PostList.jsx
+++ Next JS Training/react-crash-course/src/components/PostList.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import styles from './PostList.module.css';
 function PostList() {
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }  
+    
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
     return (
     <>
-        <NewPost />
+        <NewPost onBodyChange = {bodyChangeHandler} onAuthorChange = {authorChangeHandler}/>
         <ul className={styles.posts}>
-            <Post author ="Some dude" body = "Hello world" />
+            <Post author ={enteredAuthor} body = {enteredBody} />
             <Post author ="Some other dude" body = "Other world" />
         </ul>
     </>
