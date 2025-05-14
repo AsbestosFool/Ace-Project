6da3f024947780fb81bbcc1d465bb45c08d14f28
Mike Alamil <asbestosfool@gmail.com>
2025-05-14 11:51:46 -0500

    Prep the App for state managment

--- Next JS Training/react-crash-course/src/components/PostList.jsx
+++ Next JS Training/react-crash-course/src/components/PostList.jsx
@@ -1,11 +1,15 @@
 import Post from "./Post";
+import NewPost from "./NewPost";
 import styles from './PostList.module.css';
 function PostList() {
     return (
+    <>
+        <NewPost />
         <ul className={styles.posts}>
             <Post author ="Some dude" body = "Hello world" />
             <Post author ="Some other dude" body = "Other world" />
         </ul>
+    </>
     );
 }
 
