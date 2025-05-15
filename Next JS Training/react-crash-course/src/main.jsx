8c6329e5158f02b037c89379ef26bd23bff2aa98
Mike Alamil <asbestosfool@gmail.com>
2025-05-15 15:04:24 -0500

    Added routes

--- Next JS Training/react-crash-course/src/main.jsx
+++ Next JS Training/react-crash-course/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+
 import './index.css'
 import App from './App'
+import NewPost from './components/NewPost';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {path:'/', element: <App/>},
+  { path: '/create-posts', element: <NewPost />}
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
 )
