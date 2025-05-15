9b198302dcb7fbb7595a622ce329f21f22bcd597
Mike Alamil <asbestosfool@gmail.com>
2025-05-14 20:15:47 -0500

    Added State and Conditional Content

--- Next JS Training/react-crash-course/src/components/PostList.jsx
+++ Next JS Training/react-crash-course/src/components/PostList.jsx
@@ -4,8 +4,13 @@ import NewPost from "./NewPost";
 import Modal from "./Modal";
 import styles from './PostList.module.css';
 function PostList() {
+    const [moalIsVisible, setModalIsVisible] = useState(true);
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
 
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
@@ -16,11 +21,13 @@ function PostList() {
     }
     return (
     <>
-        <Modal>
+        {moalIsVisible ? (
+        <Modal onClose = {hideModalHandler}>
             <NewPost 
             onBodyChange = {bodyChangeHandler} 
             onAuthorChange = {authorChangeHandler}/>
         </Modal>
+        ): false}
         <ul className={styles.posts}>
             <Post author ={enteredAuthor} body = {enteredBody} />
             <Post author ="Some other dude" body = "Other world" />
