f8b404d3917bc0a531561db7b8941ab7eff1262b
Mike Alamil <asbestosfool@gmail.com>
2025-05-15 09:49:42 -0500

    Added a shared header and more state management

--- Next JS Training/react-crash-course/src/components/PostList.jsx
+++ Next JS Training/react-crash-course/src/components/PostList.jsx
@@ -3,14 +3,10 @@ import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 import styles from './PostList.module.css';
-function PostList() {
-    const [moalIsVisible, setModalIsVisible] = useState(true);
+function PostList({isPosting, onStopPosting}) {
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
 
-    function hideModalHandler() {
-        setModalIsVisible(false);
-    }
 
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
@@ -21,13 +17,13 @@ function PostList() {
     }
     return (
     <>
-        {moalIsVisible ? (
-        <Modal onClose = {hideModalHandler}>
+        {isPosting && (
+        <Modal onClose = {onStopPosting}>
             <NewPost 
             onBodyChange = {bodyChangeHandler} 
             onAuthorChange = {authorChangeHandler}/>
         </Modal>
-        ): false}
+        )}
         <ul className={styles.posts}>
             <Post author ={enteredAuthor} body = {enteredBody} />
             <Post author ="Some other dude" body = "Other world" />
