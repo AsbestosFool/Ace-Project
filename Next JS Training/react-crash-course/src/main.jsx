d4a2556679220e4dd3dcc0766450309b21c0b0ff
Mike Alamil <asbestosfool@gmail.com>
2025-05-16 08:06:39 -0500

    Added some layouts routes

--- Next JS Training/react-crash-course/src/main.jsx
+++ Next JS Training/react-crash-course/src/main.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 
-import './index.css'
+
 import App from './App'
 import NewPost from './components/NewPost';
+import RootLayout from './routes/RootLayout'; 
+import './index.css'
 
 const router = createBrowserRouter([
-  {path:'/', element: <App/>},
-  { path: '/create-posts', element: <NewPost />}
+  {
+    path:'/',
+    element: <RootLayout />,
+    children: [
+      {path:'/', element: <App/>},
+      { path: '/create-posts', element: <NewPost />},
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
