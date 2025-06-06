243e45d40d11139d9fce796fb7358ede090ed1f8
Mike Alamil <asbestosfool@gmail.com>
2025-06-06 10:32:06 -0500

    Adding UI for timer challenge

--- React Training/Refs Portals/src/App.jsx
+++ React Training/Refs Portals/src/App.jsx
@@ -1,10 +1,16 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge titile="Easy" targetTime={1} />
+        <TimerChallenge titile="Not Easy" targetTime={5} />
+        <TimerChallenge titile="Gettting tough" targetTime={10} />
+        <TimerChallenge titile="Pros only" targetTime={15} />
+      </div>
     </>
   );
 }
